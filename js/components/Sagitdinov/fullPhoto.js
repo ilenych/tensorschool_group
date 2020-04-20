464e483e9b624d3c6f672146653cfd278443158d
qwersety <63055743+qwersety@users.noreply.github.com>
2020-04-20 16:58:08 +0500

    require

--- js/components/Sagitdinov/fullPhoto.js
+++ js/components/Sagitdinov/fullPhoto.js
@@ -1,32 +1,27 @@
-class FullPhoto extends User {
-  constructor (userID, options){
-    super(userID);
-    this.options=options;
+define(['Base/Model'], function (Model) {
+   'use strict';  
+   class FullPhoto extends User {
+      constructor (userID, options){
+      super(userID);
+      this.options=options;
+    }
+
+    fullPhotoShow(){
+     let forEverything = document.getElementById('freeBlock');
+     forEverything.innerHTML += this.renderFullPhotoString;
+    }
+
+    fullPhotoDrop(){
+      let forEverything = document.getElementById('freeBlock');
+      forEverything.innerHTML = ``;
+    }
+    get renderFullPhotoString(){
+      return `<div class="fullPhotoShadow" onclick="dropFullPhoto(${this.id});">
+              <div class="fullPhotoBox">
+              <img src="img/user${this.id}/${this.options}.jpg" alt="Аватар пользователя ${this.surname} ${this.name[0]} ${this.patronymic[0]}" onclick="dropFullPhoto(${this.id});" class="fullPhoto">
+              </div></div>`;
+    }
   }
 
-  fullPhotoShow(){
-   let forEverything = document.getElementById('freeBlock');
-   forEverything.innerHTML += this.renderFullPhotoString;
-  }
-
-  fullPhotoDrop(){
-    let forEverything = document.getElementById('freeBlock');
-    forEverything.innerHTML = ``;
-  }
-  get renderFullPhotoString(){
-    return `<div class="fullPhotoShadow" onclick="dropFullPhoto(${this.id});">
-            <div class="fullPhotoBox">
-            <img src="img/user${this.id}/${this.options}.jpg" alt="Аватар пользователя ${this.surname} ${this.name[0]} ${this.patronymic[0]}" onclick="dropFullPhoto(${this.id});" class="fullPhoto">
-            </div></div>`;
-  }
-}
-
-function getFullPhoto(userID, options){
-  let forEverything = new FullPhoto(userID, options);
-  forEverything.fullPhotoShow();
-}
-
-function dropFullPhoto(userID){
-  let forEverything = new FullPhoto(userID);
-  forEverything.fullPhotoDrop();
-}
+  return FullPhoto;
+});
