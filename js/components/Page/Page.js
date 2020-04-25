300c883b7a7051240ebf2a74d128f012a4f9325c
zapikankaa <tugaevad@gmail.com>
2020-04-25 20:32:20 +0500

    profilepic and messages as components

--- js/components/Page/Page.js
+++ js/components/Page/Page.js
@@ -42,65 +42,65 @@ define([
 
                             <div class="content__secondary-column">
                                 <div class="module">
-                                    <div class="profile-photo">
-                                        <img class="page__image page__image__rounded" src="img/1.jpg">
+                                    ${this.childrens.create(ProfilePic)}
+                                </div>
+
+                                <div class="module">
+                                    <!--Модуль ссылок-->
+                                    <div class="page-links">
+                                        <div class="page-links__link">
+                                            <div class="page-links__image-link">
+                                                <img src="img/ui/link1.png" alt="" class="page-links__image">
+                                            </div>
+                                            <div class="page-links__title">
+                                                Поклонники
+                                            </div>
+                                        </div>
+                                        <div class="page-links__link">
+                                            <div class="page-links__image-link">
+                                                <img src="img/ui/link2.png" alt="" class="page-links__image">
+                                            </div>
+                                            <div class="page-links__title">
+                                                Интересное
+                                            </div>
+                                        </div>
+                                        <div class="page-links__link">
+                                            <div class="page-links__image-link">
+                                                <img src="img/ui/link3.png" alt="" class="page-links__image">
+                                            </div>
+                                            <div class="page-links__title">
+                                                Друзья
+                                            </div>
+                                        </div>
+                                        <div class="page-links__link">
+                                            <div class="page-links__image-link">
+                                                <img src="img/ui/link4.png" alt="" class="page-links__image">
+                                            </div>
+                                            <div class="page-links__title">
+                                                Видосы
+                                            </div>
+                                        </div>
+                                        <div class="page-links__link">
+                                            <div class="page-links__image-link">
+                                                <img src="img/ui/link5.png" alt="" class="page-links__image">
+                                            </div>
+                                            <div class="page-links__title">
+                                                Фотки
+                                            </div>
+                    
+                                        </div>
+                                        <div class="page-links__link">
+                                            <div class="page-links__image-link">
+                                                <img src="img/ui/link6.png" alt="" class="page-links__image">
+                                            </div>
+                                            <div class="page-links__title">
+                                                Музыка
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="module">
-
-                                    <!--Модуль ссылок-->
-                                    <div class="page-links">
-                                    <div class="page-links__link">
-                                        <div class="page-links__image-link">
-                                            <img src="img/ui/link1.png" alt="" class="page-links__image">
-                                        </div>
-                                        <div class="page-links__title">
-                                            Поклонники
-                                        </div>
-                                    </div>
-                                    <div class="page-links__link">
-                                        <div class="page-links__image-link">
-                                            <img src="img/ui/link2.png" alt="" class="page-links__image">
-                                        </div>
-                                        <div class="page-links__title">
-                                            Интересное
-                                        </div>
-                                    </div>
-                                    <div class="page-links__link">
-                                        <div class="page-links__image-link">
-                                            <img src="img/ui/link3.png" alt="" class="page-links__image">
-                                        </div>
-                                        <div class="page-links__title">
-                                            Друзья
-                                        </div>
-                                    </div>
-                                    <div class="page-links__link">
-                                        <div class="page-links__image-link">
-                                            <img src="img/ui/link4.png" alt="" class="page-links__image">
-                                        </div>
-                                        <div class="page-links__title">
-                                            Видосы
-                                        </div>
-                                    </div>
-                                    <div class="page-links__link">
-                                        <div class="page-links__image-link">
-                                            <img src="img/ui/link5.png" alt="" class="page-links__image">
-                                        </div>
-                                        <div class="page-links__title">
-                                            Фотки
-                                        </div>
-                
-                                    </div>
-                                    <div class="page-links__link">
-                                        <div class="page-links__image-link">
-                                            <img src="img/ui/link6.png" alt="" class="page-links__image">
-                                        </div>
-                                        <div class="page-links__title">
-                                            Музыка
-                                        </div>
-                                    </div>
-                                </div>
-
+                                    ${this.childrens.create(Messages)}
                                 </div>
                             </div>
                         </div>
