1fd8df22e33334e041cac69e278d66c5dcd90b05
ilenych <ilenych6@gmail.com>
2020-05-01 01:44:30 +0500

    Adde fecher manager and delete default modeel in Wall.js

--- js/components/Ilenko/wall.js
+++ js/components/Ilenko/wall.js
@@ -1,10 +1,37 @@
-define(["Base/Component", "Wall/model", "Wall/post", "Wall/NetworkService"], function (
+define(["Base/Component", "Wall/post", "Wall/NetworkService"], function (
   Component,
-  model,
   Post,
   NetworkService
 ) {
   "use strict";
+
+  class FetcherManager extends Component {
+    constructor({ item }) {
+      super();
+      this.state.item = item;
+      this.options.view;
+    }
+
+    beforeMount() {
+      NetworkService.fetchData().then((res) => {
+        this.state.item = res;
+        this.update();
+      });
+    }
+
+    beforeUpdate() {
+      this.getContainer().innerHTML = "";
+
+      this.options.view = this.childrens.create(Wall, this.state);
+      this.options.view.mount(this.getContainer());
+    }
+
+    render() {
+      return `<div class="main-wall">
+                ${this.options.view || ""}
+              </div>`;
+    }
+  }
 
   class Wall extends Component {
     constructor({ item }) {
@@ -12,106 +39,17 @@ define(["Base/Component", "Wall/model", "Wall/post", "Wall/NetworkService"], fun
       this.state.item = item;
     }
 
-    beforeMount() {
-      NetworkService.fetchData().then((res) => {
-        this.state.item = res
-        this.update()
-      })
-      
-      if (this.state.item === undefined) {
-        //Стандартная модель если не передали ничего в конструктор
-        this.state.item = [
-            {
-              id: 1,
-              userName: "Джилл Валентайн",
-              userUrlImage: "img/post/ava.png",
-              time: "2020-04-21T15:59:40.007Z",
-              postText:
-                "Сыграем в RE3 remake? 3 апреля 2020 года Capcom выпустила Resident Evil 3 Remake — обновленную версию популярной игры Resident Evil 3: Nemesis 1999 года.",
-              postUrlImage: "img/post/postImage.jpg",
-              likes: {
-                likeFire: 0,
-                likeHeartEyes: 0,
-                likeRocket: 0,
-                likeLike: 0,
-                likeBomb: 0
-              },
-              comments: [],
-            },
-            {
-              id: 2,
-              userName: "Джилл Валентайн",
-              userUrlImage: "img/post/ava.png",
-              time: "2020-04-21T15:59:40.007Z",
-              postText:
-                "Сыграем в RE3 remake? 3 апреля 2020 года Capcom выпустила Resident Evil 3 Remake — обновленную версию популярной игры Resident Evil 3: Nemesis 1999 года.",
-              postUrlImage: "img/post/postImage.jpg",
-              likes: {
-                likeFire: 132,
-                likeHeartEyes: 24,
-                likeRocket: 26,
-                likeLike: 68,
-                likeBomb: 87
-              },
-              comments: [ {
-                userUrlImage: "img/post/newUser.png",
-                userName: "Леон Скотт Кеннеди",
-                commentText: "Нашу встречу вырезалии с тобой (",
-                commentTime: "2020-04-22T15:59:40.007Z",
-              },
-              {
-                userUrlImage: "img/post/newUser.png",
-                userName: "Леон Скотт Кеннеди",
-                commentText: "Нашу встречу вырезалии с тобой (",
-                commentTime: "2020-04-22T15:59:40.007Z",
-              },
-              {
-                userUrlImage: "img/post/newUser.png",
-                userName: "Леон Скотт Кеннеди",
-                commentText: "Нашу встречу вырезалии с тобой (",
-                commentTime: "2020-04-22T15:59:40.007Z",
-              },
-              {
-                userUrlImage: "img/post/newUser.png",
-                userName: "Леон Скотт Кеннеди",
-                commentText: "Нашу встречу вырезалии с тобой (",
-                commentTime: "2020-04-22T15:59:40.007Z",
-              },
-              {
-                userUrlImage: "img/post/newUser.png",
-                userName: "Леон Скотт Кеннеди",
-                commentText: "Нашу встречу вырезалии с тобой (",
-                commentTime: "2020-04-22T15:59:40.007Z",
-              }
-              
-            ],
-            },
-          ]
-      }
-    }
-
-    beforeUpdate() {
-      console.log('update')
-      // this._wall = this.getContainer().innerHTML = this.render(this.options, this.state)
-      
-      this.beforeUnmount()
-    }
-
-    beforeUnmount() {
-      delete this._wall
-    }
-
     render(options, { item }) {
       return `<div class="wall">
                 ${item.map(this.renderPost.bind(this)).join("\n")}
-                </div>`;
+              </div>`;
     }
 
     renderPost(item) {
       return `<div class="module">
-                    ${this.childrens.create(Post, { item })} 
-                </div>`;
+                ${this.childrens.create(Post, { item })} 
+              </div>`;
     }
   }
-  return Wall;
+  return FetcherManager;
 });
