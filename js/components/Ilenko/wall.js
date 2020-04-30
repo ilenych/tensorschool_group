add9a81d22c8b6fc1f5fe6e75599cced2319ad0d
ilenych <ilenych6@gmail.com>
2020-04-30 21:36:51 +0500

    Refactoring code in Wall.js

--- js/components/Ilenko/wall.js
+++ js/components/Ilenko/wall.js
@@ -20,8 +20,7 @@ define(["Base/Component", "Wall/model", "Wall/post", "Wall/NetworkService"], fun
       
       if (this.state.item === undefined) {
         //Стандартная модель если не передали ничего в конструктор
-        this.state.item = {
-          wall: [
+        this.state.item = [
             {
               id: 1,
               userName: "Джилл Валентайн",
@@ -30,37 +29,14 @@ define(["Base/Component", "Wall/model", "Wall/post", "Wall/NetworkService"], fun
               postText:
                 "Сыграем в RE3 remake? 3 апреля 2020 года Capcom выпустила Resident Evil 3 Remake — обновленную версию популярной игры Resident Evil 3: Nemesis 1999 года.",
               postUrlImage: "img/post/postImage.jpg",
-              likeFire: 132,
-              likeHeartEyes: 24,
-              likeRocket: 26,
-              likeLike: 68,
-              likeBomb: 87,
-              comments: [
-                {
-                  userUrlImage: "img/post/newUser.png",
-                  userName: "Леон Скотт Кеннеди",
-                  commentText: "Нашу встречу вырезалии с тобой (",
-                  commentTime: "2020-04-22T15:59:40.007Z",
-                },
-                {
-                  userUrlImage: "img/post/ava.png",
-                  userName: "Фэйк Джилл",
-                  commentText: "Так может быть ты...",
-                  commentTime: "2020-04-22T15:59:40.007Z",
-                },
-                {
-                  userUrlImage: "img/post/ava.png",
-                  userName: "Фэйк Джилл",
-                  commentText: "Так может быть ты...",
-                  commentTime: "2020-04-22T15:59:40.007Z",
-                },
-                {
-                  userUrlImage: "img/post/ava.png",
-                  userName: "Фэйк Джилл",
-                  commentText: "Так может быть ты...",
-                  commentTime: "2020-04-22T15:59:40.007Z",
-                },
-              ],
+              likes: {
+                likeFire: 0,
+                likeHeartEyes: 0,
+                likeRocket: 0,
+                likeLike: 0,
+                likeBomb: 0
+              },
+              comments: [],
             },
             {
               id: 2,
@@ -70,35 +46,55 @@ define(["Base/Component", "Wall/model", "Wall/post", "Wall/NetworkService"], fun
               postText:
                 "Сыграем в RE3 remake? 3 апреля 2020 года Capcom выпустила Resident Evil 3 Remake — обновленную версию популярной игры Resident Evil 3: Nemesis 1999 года.",
               postUrlImage: "img/post/postImage.jpg",
-              likeFire: 132,
-              likeHeartEyes: 24,
-              likeRocket: 26,
-              likeLike: 68,
-              likeBomb: 87,
-              comments: [
-                {
-                  userUrlImage: "img/post/newUser.png",
-                  userName: "Леон Скотт Кеннеди",
-                  commentText: "Нашу встречу вырезалии с тобой (",
-                  commentTime: "2020-04-22T15:59:40.007Z",
-                },
-                {
-                  userUrlImage: "img/post/ava.png",
-                  userName: "Фэйк Джилл",
-                  commentText: "Так может быть ты...",
-                  commentTime: "2020-04-22T15:59:40.007Z",
-                },
-              ],
+              likes: {
+                likeFire: 132,
+                likeHeartEyes: 24,
+                likeRocket: 26,
+                likeLike: 68,
+                likeBomb: 87
+              },
+              comments: [ {
+                userUrlImage: "img/post/newUser.png",
+                userName: "Леон Скотт Кеннеди",
+                commentText: "Нашу встречу вырезалии с тобой (",
+                commentTime: "2020-04-22T15:59:40.007Z",
+              },
+              {
+                userUrlImage: "img/post/newUser.png",
+                userName: "Леон Скотт Кеннеди",
+                commentText: "Нашу встречу вырезалии с тобой (",
+                commentTime: "2020-04-22T15:59:40.007Z",
+              },
+              {
+                userUrlImage: "img/post/newUser.png",
+                userName: "Леон Скотт Кеннеди",
+                commentText: "Нашу встречу вырезалии с тобой (",
+                commentTime: "2020-04-22T15:59:40.007Z",
+              },
+              {
+                userUrlImage: "img/post/newUser.png",
+                userName: "Леон Скотт Кеннеди",
+                commentText: "Нашу встречу вырезалии с тобой (",
+                commentTime: "2020-04-22T15:59:40.007Z",
+              },
+              {
+                userUrlImage: "img/post/newUser.png",
+                userName: "Леон Скотт Кеннеди",
+                commentText: "Нашу встречу вырезалии с тобой (",
+                commentTime: "2020-04-22T15:59:40.007Z",
+              }
+              
+            ],
             },
-          ],
-        };
+          ]
       }
     }
 
     beforeUpdate() {
-      console.log(this)
+      console.log('update')
       // this._wall = this.getContainer().innerHTML = this.render(this.options, this.state)
-      // this.beforeUnmount()
+      
+      this.beforeUnmount()
     }
 
     beforeUnmount() {
@@ -107,7 +103,7 @@ define(["Base/Component", "Wall/model", "Wall/post", "Wall/NetworkService"], fun
 
     render(options, { item }) {
       return `<div class="wall">
-                ${item.wall.map(this.renderPost.bind(this)).join("\n")}
+                ${item.map(this.renderPost.bind(this)).join("\n")}
                 </div>`;
     }
 
