9267c8779fa14909267d2f06c86eaff825a23a79
ilenych <ilenych6@gmail.com>
2020-05-17 12:50:19 +0500

    Update wall.js

--- js/components/Ilenko/wall.js
+++ js/components/Ilenko/wall.js
@@ -18,25 +18,106 @@ define(["Base/Component", "Wall/post", "Wall/NetworkService"], function (
       });
     }
 
+    afterMount() {
+      //AddEventListener на кнопку "опубликовать"
+      setTimeout(
+        function () {
+          this._publish = document.querySelector(
+            ".createPost-buttons__publish"
+          );
+          this.subscribeTo(
+            this._publish,
+            "click",
+            this.onClickButtonPublish.bind(this)
+          );
+        }.bind(this),
+        1000
+      );
+    }
+
+    onClickButtonPublish() {
+      setTimeout(
+        function () {
+          NetworkService.fetchData().then((res) => {
+            this.sortArray(res);
+          });
+        }.bind(this),
+        2000
+      );
+    }
+
     /**
      * Сортирует массив комментариев по постам
      * @param {json} content - база данных в json формате
      */
     sortArray(content) {
       let arr = [];
+      let id = [];
+      //Проверка массива на пустоту
+      if (content.comments.length == 0) {
+        content.comments = [{}];
+      }
+      //Берем id элементов wall
+      for (let i in content.wall) {
+        id.push(content.wall[i].id);
+      }
+      //Бурем значения из массива
       for (let i in content.comments) {
+        //Цикл по количеству постов
         for (let n = 0; n < content.wall.length; n++) {
-          let number = n + 1;
-          if (content.comments[i].wallId == number) {
+          //Если wallId равен числу
+          if (content.comments[i].wallId == id[n]) {
+            //Если массив от числа еще не создан и равен undefind
             if (arr[n] == undefined) {
+              //То создаем новый массив
               arr[n] = new Array(content.comments[i]);
+              //А если есть массив уже
             } else {
+              //То добавляем в существуюший
               arr[n].push(content.comments[i]);
             }
           }
+          //Если постов нет
+          if (arr[n] == undefined) {
+            //Создаем пустой массив
+            arr[n] = new Array();
+          }
         }
       }
+      this.createLikes(content);
       this.createModel(content, arr);
+    }
+
+    /**
+     * Создает модель для likes и пушит на сервер
+     * @param {json} content -  база данных в json формате
+     */
+    createLikes(content) {
+      let id = [];
+      //Берем id элементов wall
+      for (let i in content.wall) {
+        id.push(content.wall[i].id);
+      }
+
+      for (let i = 0; i < content.wall.length; i++) {
+        //Если нет по i модели лайков
+        if (content.likes[i] == undefined) {
+          //Создаем модель лайков
+          let defaultLike = {
+            modelId: id[i],
+            likeFire: 0,
+            likeHeartEyes: 0,
+            likeRocket: 0,
+            likeLike: 0,
+            likeBomb: 0,
+            id: id[i],
+          };
+          //Отправляем на сервер
+          NetworkService.postDataLikes(defaultLike);
+          //Добавляем в массив
+          content.likes[i] = defaultLike;
+        }
+      }
     }
     /**
      * Создает модель для отображения данных
@@ -55,9 +136,11 @@ define(["Base/Component", "Wall/post", "Wall/NetworkService"], function (
           postUrlImage: content.wall[i].postUrlImage,
           likes: content.likes[i],
           comments: comments[i],
+          //TODO: Заменить в будущем
+          user: content.users[0],
         });
       }
-      this.state.item = arr;
+      this.state.item = arr.reverse();
       this.update();
     }
 
