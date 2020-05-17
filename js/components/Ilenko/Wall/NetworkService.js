18ca837ae91840d2654f55e01791d4556c1cfad6
ilenych <ilenych6@gmail.com>
2020-05-17 12:52:06 +0500

    Update NetworkService.js

--- js/components/Ilenko/Wall/NetworkService.js
+++ js/components/Ilenko/Wall/NetworkService.js
@@ -1,6 +1,6 @@
 define({
   /**
-   * Возвращает данные с http://localhost:3000/db
+   * Возвращает все данные с сервера
    */
   fetchData: async function () {
     try {
@@ -12,9 +12,24 @@ define({
     }
   },
   /**
-   * Пушит данные с комментами в http://localhost:3000/comments
+   * Получение комментариев по id
+   * @param {Number} id - id элемента
    */
-  postData: async function (comment) {
+  getDataComments: async function (id) {
+    try {
+      let responce = await fetch(
+        "https://tensorschool.herokuapp.com/wall/" + id + "/comments"
+      );
+      let content = await responce.json();
+      return content;
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  /**
+   * Пушит данные с комментами в https://tensorschool.herokuapp.com/comments
+   */
+  postDataComment: async function (comment) {
     try {
       await fetch("https://tensorschool.herokuapp.com/comments", {
         method: "POST",
@@ -28,15 +43,12 @@ define({
     }
   },
   /**
-   * Удаляет данный пост с http://localhost:3000/model/id
-   * id
+   * Удаляет данные с сервера по id
+   * @param {Number} id - id элемента
    */
   deleteData: async function (id) {
     try {
       await fetch("https://tensorschool.herokuapp.com/wall/" + id, {
-        method: "DELETE",
-      });
-      await fetch("https://tensorschool.herokuapp.com/wall/" + id + "/comments", {
         method: "DELETE",
       });
       await fetch("https://tensorschool.herokuapp.com/likes/" + id, {
@@ -47,9 +59,22 @@ define({
     }
   },
   /**
-   * Заменяет данные лайков в http://localhost:3000/like/id
+   * Удаляет комментарии с сервера по wallId
+   * @param {Number} id - id элемента
    */
-  putData: async function (id, likes) {
+  deleteDataComment: async function (id) {
+    try {
+      await fetch("https://tensorschool.herokuapp.com/comments/" + id, {
+        method: "DELETE",
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  /**
+   * Заменяет данные лайков на сервера в зависиимости от id
+   */
+  putDataLikes: async function (id, likes) {
     try {
       await fetch("https://tensorschool.herokuapp.com/likes/" + id, {
         method: "PUT",
@@ -62,14 +87,50 @@ define({
       console.error(err);
     }
   },
-
-  // getDataUser: async function () {
-  //   try {
-  //     let responce = await fetch("http://localhost:3000/users/1");
-  //     let content = await responce.json();
-  //     return content;
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // },
+  /**
+   * Пушит модель данных лайков на сервер
+   * @param {Object} likes - модель лайков
+   */
+  postDataLikes: async function (likes) {
+    try {
+      await fetch("https://tensorschool.herokuapp.com/likes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(likes),
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  /**
+   * Пущит модель данный нового поста на сервер
+   * @param {Object} post
+   */
+  postData: async function (post) {
+    try {
+      await fetch("https://tensorschool.herokuapp.com/wall", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(post),
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  /**
+   * Получение данных о пользователе
+   */
+  getDataUser: async function () {
+    try {
+      let responce = await fetch("https://tensorschool.herokuapp.com/users/1");
+      let content = await responce.json();
+      return content;
+    } catch (err) {
+      console.error(err);
+    }
+  },
 });
