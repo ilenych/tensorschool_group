0d75660c9f58b0851fcedb1bae227a74a261b0ff
ilenych <ilenych6@gmail.com>
2020-05-20 17:05:54 +0500

    Add component

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ requirejs.config({
         Page: 'components/Page',
         Wall: 'components/Ilenko/wall', // Иленко Алексей
         CreatePost: 'components/Ilenko/CreatePost', // Иленко Алексей
+        Gallery: 'components/Ilenko/Gallery', // Иленко Алексей
         Modul2: 'components/Timofeev/Modul2', // Тимофеев Павел
         ProfilePic: 'components/Tugaeva/profilepic', // Тугаева Динара
         Messages: 'components/Tugaeva/messages', // Тугаева Динара
