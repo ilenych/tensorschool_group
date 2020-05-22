139d4f51fad50bd9a996a3c018989931b8781cab
Golden4 <g.axe@list.ru>
2020-05-22 16:02:28 +0500

    Update header.js

--- js/components/Saitov/header.js
+++ js/components/Saitov/header.js
@@ -77,7 +77,7 @@ define([
                             ${state.item.getActivity()}
                         </p>
                         <a class="header__profile-edit">Редактировать</a>
-                        <a class="header__profile-menu" href="/">
+                        <a class="header__profile-menu" href="${location.host}">
                             <img class="header__profile-avatar header__profile-avatar_mini" src="https://tensor-school.herokuapp.com/user/photo/${state.id}" alt="avatar">
                             <span class="header__profile-button">&#8942;</span>
                         </a>
