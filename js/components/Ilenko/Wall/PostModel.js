662f1d145c084e658fe46d78c26b1d12dafafc6e
ilenych <ilenych6@gmail.com>
2020-05-23 16:56:06 +0500

    Update postModel.js

--- js/components/Ilenko/Wall/PostModel.js
+++ js/components/Ilenko/Wall/PostModel.js
@@ -1,106 +1,97 @@
-define(["Base/Model", "components/Ilenko/Service/NetworkService"], function (Model, NetworkService) {
-    class PostModel extends Model {
-
-      getContent() {
-        return this.sortArray();
-      }
-
-      sortArray() {
-        let arr = [];
-        let id = [];
-        //Проверка массива на пустоту
-        if (this.comments.length == 0) {
-          this.comments = [{}];
-        }
-        //Берем id элементов wall
-        for (let i in this.wall) {
-          id.push(this.wall[i].id);
-        }
-        //Бурем значения из массива
-        for (let i in this.comments) {
-          //Цикл по количеству постов
-          for (let n = 0; n < this.wall.length; n++) {
-            //Если wallId равен числу
-            if (this.comments[i].wallId == id[n]) {
-              //Если массив от числа еще не создан и равен undefind
-              if (arr[n] == undefined) {
-                //То создаем новый массив
-                arr[n] = new Array(this.comments[i]);
-                //А если есть массив уже
-              } else {
-                //То добавляем в существуюший
-                arr[n].push(this.comments[i]);
-              }
-            }
-            //Если постов нет
-            if (arr[n] == undefined) {
-              //Создаем пустой массив
-              arr[n] = new Array();
-            }
-          }
-        }
-        this.createLikes();
-        return this.createModel(arr);
-      }
-  
-      /**
-       * Создает модель для likes и пушит на сервер
-       * @param {json} this -  база данных в json формате
-       */
-      createLikes() {
-        let id = [];
-        //Берем id элементов wall
-        for (let i in this.wall) {
-          id.push(this.wall[i].id);
-        }
-  
-        for (let i = 0; i < this.wall.length; i++) {
-          //Если нет по i модели лайков
-          if (this.likes[i] == undefined) {
-            //Создаем модель лайков
-            let defaultLike = {
-              modelId: id[i],
-              likeFire: 0,
-              likeHeartEyes: 0,
-              likeRocket: 0,
-              likeLike: 0,
-              likeBomb: 0,
-              id: id[i],
-            };
-            //Отправляем на сервер
-            NetworkService.postDataLikes(defaultLike);
-            //Добавляем в массив
-            this.likes[i] = defaultLike;
-          }
-        }
-      }
-      /**
-       * Создает модель для отображения данных
-       * @param {json} this -  база данных в json формате
-       * @param {Array} comments - отсортированный массив комментариев
-       */
-      createModel(comments) {
-        let arr = [];
-        for (let i = 0; i < this.wall.length; i++) {
-          arr.push({
-            id: this.wall[i].id,
-            userName: this.wall[i].userName,
-            userUrlImage: this.wall[i].userUrlImage,
-            time: this.wall[i].time,
-            postText: this.wall[i].postText,
-            postUrlImage: this.wall[i].postUrlImage,
-            likes: this.likes[i],
-            comments: comments[i],
-            //TODO: Заменить в будущем
-            user: this.users[0],
-          });
-        }
-        return arr.reverse();
-      }
-  
-
+define(["Base/Model", "components/Ilenko/Service/NetworkService"], function (
+  Model,
+  NetworkService
+) {
+  class PostModel extends Model {
+    getContent() {
+      return this.sortArray();
     }
-  
-    return PostModel;
-  });
-  
+
+    sortArray() {
+      //вспомогательый массив
+      let arr = [];
+      //модель
+      let content = {
+        posts: [],
+        comments: [],
+        likes: [],
+      };
+      //Закидываем данные в вспомогательный массив
+      for (let i in this) {
+        arr.push(this[i]);
+      }
+      //Сортируем массив по модели content
+      for (let i in arr) {
+        if (arr[i].postText != undefined) {
+          content.posts.push(arr[i]);
+        } else if (arr[i].modelId != undefined) {
+          content.likes.push(arr[i]);
+        } else content.comments.push(arr[i]);
+      }
+      //Проверка на налиичие постов
+      for(let i in content.comments){
+         // Если постов нет
+         if (!Array.isArray(content.comments[i])) {
+          //Создаем пустой массив
+          content.comments[i] = new Array();
+        }
+      }
+      return this.createLikes(content);
+    }
+
+    /**
+     * Создает модель для likes и пушит на сервер
+     * * @param {Array} content - [posts: [...], comments: [...], likes: [...]]
+     */
+    createLikes(content) {
+      let id = [];
+      //Берем id элементов wall
+      for (let i in content.posts) {
+        id.push(content.posts[i].id);
+      }
+
+      for (let i = 0; i < content.posts.length; i++) {
+        //Если нет по i модели лайков
+        if (content.likes[i] == undefined) {
+          //Создаем модель лайков
+          let defaultLike = {
+            modelId: id[i],
+            likeFire: 0,
+            likeHeartEyes: 0,
+            likeRocket: 0,
+            likeLike: 0,
+            likeBomb: 0,
+            id: id[i],
+          };
+          //Отправляем на сервер
+          NetworkService.postDataLikes(defaultLike);
+          //Добавляем в массив
+          content.likes[i] = defaultLike;
+        }
+      }
+      return this.createModel(content);
+    }
+    /**
+     * Создает модель для отображения данных
+     * @param {Array} content - [posts: [...], comments: [...], likes: [...]]
+     */
+    createModel(content) {
+      let arr = [];
+      for (let i = 0; i < content.posts.length; i++) {
+        arr.push({
+          id: content.posts[i].id,
+          userName: content.posts[i].userName,
+          userUrlImage: content.posts[i].userUrlImage,
+          time: content.posts[i].time,
+          postText: content.posts[i].postText,
+          postUrlImage: content.posts[i].postUrlImage,
+          likes: content.likes[i],
+          comments: content.comments[i],
+        });
+      }
+      return arr.reverse();
+    }
+  }
+
+  return PostModel;
+});
