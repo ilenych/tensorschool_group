42a52922e6944ca398e1a702837b957ce0666ad2
Golden4 <g.axe@list.ru>
2020-05-26 16:07:22 +0500

    AddThemes

--- js/components/Base/Theme.js
+++ js/components/Base/Theme.js
@@ -7,17 +7,29 @@ define([
             this.curTheme = 0;
 
             this.themes = [{
-                name: 'default',
+                name: 'Стандартная (синяя)',
                 backgroundHeaderColor: '#4a76a8',
                 backgroundHeaderSecondaryColor: '#224b7a',
                 linkColor: '#4da3ff'
             },
             {
-                name: 'violet',
+                name: 'Фиолетовая',
                 backgroundHeaderColor: '#924aa8',
                 backgroundHeaderSecondaryColor: '#6b227a',
                 linkColor: '#804aa8'
             },
+            {
+                name: 'Красная',
+                backgroundHeaderColor: '#a84a4a',
+                backgroundHeaderSecondaryColor: '#7a2222',
+                linkColor: '#a84a4a'
+            },
+            {
+                name: 'Зеленая',
+                backgroundHeaderColor: '#4fa84a',
+                backgroundHeaderSecondaryColor: '#3c7a22',
+                linkColor: '#4aa857'
+            }
             ];
         }
 
